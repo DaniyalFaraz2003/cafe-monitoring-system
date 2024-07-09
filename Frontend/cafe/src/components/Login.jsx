3d2f319e62ae142372b0efba828c7670e1081a6a
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-10 02:22:42 +0500

    changes

--- Frontend/cafe/src/components/Login.jsx
+++ Frontend/cafe/src/components/Login.jsx
@@ -1,51 +1,25 @@
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { Link, useNavigate } from 'react-router-dom'
-// import authService from '../appwrite/auth'
-// import { login as authLogin } from '../store/authSlice'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 
 function Login() {
-  // const dispatch = useDispatch()
-  // const { register, handleSubmit } = useForm()
-  // const [error, setError] = useState("")
-  // const navigate = useNavigate()
-  // const login = async (data) => {
-  //   setError("")
-  //   try {
-  //     const session =await authService.login(data)
-  //     if (session) {
-  //       const userData = await authService.getCurrentUser()
-  //       if (userData) dispatch(authLogin(userData))
-  //       navigate("/")
-  //     }
-  //   } catch (error) {
-  //     setError(error.message)
-  //     console.log("error.message", error.message)
-  //   }
-  // }
+
   return (
     <div className="model">
     <div className="innermodel">
       <h2>Sign in to your account</h2>
       <p>Don't have any account
         <Link to="/Signup" style={{textDecoration:"none"}}>Sign Up</Link></p>
-      {/* {error && <p>{error}</p>} */}
      
       <form 
-      // onSubmit={handleSubmit(login)}
        >
         <div className="input-field">
           <input label="email" placeholder="Email" 
-          // {...register("email", {
-          //   required: true,
-          //   matchPatern: (value) => /^\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Invalid email"
-          // })}
            />
         </div>
         <div className="input-field">
           <input label="password" placeholder="Password"
-          //  {...register("password", { required: true })} 
            />
         </div>
         <button className="submit-btn" type='submit'>Sign in</button>
--- Frontend/cafe/src/components/Signup.jsx
+++ Frontend/cafe/src/components/Signup.jsx
@@ -1,62 +1,29 @@
-import React, { useState } from 'react'
-// import authService from '../appwrite/auth'
-import { Link, useNavigate } from 'react-router-dom'
-// import { login } from '../store/authSlice'
-import { useDispatch } from 'react-redux'
-import { useForm } from 'react-hook-form'
-function Signup() {
-    // const dispatch = useDispatch()
-    // const { register, handleSubmit } = useForm()
-    // const [error, setError] = useState("")
-    // const navigate = useNavigate()
+import { Link} from 'react-router-dom'
 
-    // const create = async (data) => {
-    //     setError("");
-    //     try {
-    //       const userData = await authService.createAccount(data);
-    //       if (userData) {
-    //         console.log(userData, "collected");
-    //         const userData2 = await authService.getCurrentUser();
-    //         if (userData2) {
-    //           console.log("second data: ", userData2);
-    //           dispatch(login(userData2));
-    //         }
-    //         navigate("/");
-    //       }
-    //     } catch (error) {
-    //       setError(error.message);
-    //     }
-    //   };
+function Signup() {
+    
     return (
       <div className='model'>
         <div className="innermodel">
-            {/* <Logo /> */}
             <h2>Sign up to create account</h2>
             <p>Already have an account
                 <Link to="/Login" 
                 style={{textDecoration:"none"}}
                 >Sign In</Link></p>
-            {/* {error && console.log(error)} */}
 
             <form 
-            // onSubmit={handleSubmit(create)}
             >
                 <div className="input-field">
                     <input label="Name: " placeholder="enter you Name" 
-                    // {...register("name", { required: true })}
                      />
                 </div>
                 <div className="input-field">
                     <input label="Email: " placeholder="Email" 
-                    // {...register("email", {
-                    //     required: true,
-                    //     matchPatern: (value) => /^\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Invalid email"
-                    // })} 
+                  
                     />
                 </div>
                 <div className="input-field">
                     <input type="password" placeholder="Password" 
-                    // {...register("password", { required: true })}
                      />
                 </div>
                 <button className="submit-btn" type='submit'>Create account</button>
