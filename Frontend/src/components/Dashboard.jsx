7c99e6241b8e4ebb0cd78990b9cb5d0c26690354
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-07-10 23:40:05 +0500

    made logout button in dashboard

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -1,12 +1,19 @@
 // src/components/Dashboard.jsx
 import React, { useEffect, useState } from 'react';
+import logout from '../assets/logout.png'
 import axios from 'axios';
 
 const Dashboard = () => {
-  
+
   return (
-    <div>
-      <h2 className="text-3xl font-bold">Dashboard</h2>
+    <div className='w-full h-full p-10'>
+      <nav className='w-full flex flex-row'>
+        <h2 className='text-3xl font-bold text-center text-white'>Admin Dashboard</h2>
+        <div className='ml-auto flex flex-row gap-2 items-center justify-center p-2 bg-red-200 rounded-lg'>
+          <p className='text-xl mr-0'>LogOut</p>
+          <img src={logout} alt="" className='h-6 w-6' />
+        </div>
+      </nav>
     </div>
   );
 };
