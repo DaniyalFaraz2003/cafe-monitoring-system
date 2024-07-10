4536034285fc7dc0d52b666a5b68fc008ee4cbdc
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-10 17:04:24 +0500

    theme changes

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import img from "../images/foodpic.png";
 function Landing() {
   return (
-      <div className="container">
-        <h1 className="landing-title">Welcome to Cafe Monitoring System</h1>
-        <p className="landing-description">
-          This is a system that helps you monitor your cafe and manage your
-          customers efficiently.
-        </p>
+    <div className="container">
+      <h1 className="landing-title">Welcome to Cafe Monitoring System</h1>
+      <p className="landing-description">
+        This is a system that helps you monitor your cafe and manage your
+        customers efficiently.
+      </p>
+      <div className="pic">
+        <img src={img} alt="" />
+      </div>
     </div>
   );
 }
