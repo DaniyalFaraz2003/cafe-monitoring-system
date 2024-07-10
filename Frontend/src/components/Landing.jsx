c40c6ae6e6c4e92067feeee1a4e20d2adc2d96d6
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-10 21:11:55 +0500

    login updated

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import img from "../images/foodpic.png";
 function Landing() {
   return (
-    <div className="container">
+    <div className="landing-container">
       <h1 className="landing-title">Welcome to Cafe Monitoring System</h1>
       <p className="landing-description">
         This is a system that helps you monitor your cafe and manage your
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -6,26 +6,49 @@ import { Link } from 'react-router-dom'
 function Login() {
 
   return (
-    <div className="model">
-    <div className="innermodel">
-      <h2>Sign in to your account</h2>
-      <p>Don't have any account
-        <Link to="/Signup" style={{textDecoration:"none"}}>Sign Up</Link></p>
-     
-      <form 
-       >
-        <div className="input-field">
-          <input label="email" placeholder="Email" 
-           />
+    
+
+<div className="container">
+  <div className="screen">
+    <div className="screen__content">
+      <form id="loginForm" className="login">
+        <div className="login__field">
+          <i className="login__icon fas fa-user"></i>
+          <input type="text" id="user_id" name="user_id" className="login__input" placeholder="User name / Email" required />
         </div>
-        <div className="input-field">
-          <input label="password" placeholder="Password"
-           />
+        <div className="login__field">
+          <i className="login__icon fas fa-lock"></i>
+          <input type="password" id="password" name="password" className="login__input" placeholder="Password" required />
         </div>
-        <button className="submit-btn" type='submit'>Sign in</button>
+        <button id="submit" className="button login__submit">
+          <span className="button__text">Log In</span>
+          <i className="button__icon fas fa-chevron-right"></i>
+        </button>
+        <p className='alert'>Already have an account </p><Link to="/SignUp" className="login__forgot">Signup</Link>
+        <br /><br />
+        <input type="checkbox" id="remember" name="remember" /> Remember Me
       </form>
+      <div className="social-login">
+        <h3>log in via</h3>
+        <div className="social-icons">
+          <a href="#" className="social-login__icon fab fa-instagram"></a>
+          <a href="#" className="social-login__icon fab fa-facebook"></a>
+          <a href="#" className="social-login__icon fab fa-twitter"></a>
+        </div>
+      </div>
     </div>
+    <div className="screen__background">
+      <span className="screen__background__shape screen__background__shape4"></span>
+      <span className="screen__background__shape screen__background__shape3"></span>
+      <span className="screen__background__shape screen__background__shape2"></span>
+      <span className="screen__background__shape screen__background__shape1"></span>
     </div>
+  </div>
+</div>
+
+
+
+    
   )
 }
 
