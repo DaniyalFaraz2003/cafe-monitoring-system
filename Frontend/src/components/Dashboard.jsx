5f5baac52090b326736357c74711479f7bb6d847
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-07-11 10:34:32 +0500

    minor changes

--- Frontend/src/components/Dashboard.jsx
+++ Frontend/src/components/Dashboard.jsx
@@ -14,11 +14,11 @@ const Dashboard = () => {
           <img src={logout} alt="" className='h-6 w-6' />
         </div>
       </nav>
-      <div className='w-full flex p-10 flex-row h-52 gap-10 bg-white'>
-        <div className='basis-1/3 h-full bg-black'>
+      <div className='w-full flex p-5 flex-row h-52 gap-5 bg-[#ea7c69] rounded-lg mt-10'>
+        <div className='basis-1/3 h-full rounded-xl bg-white justify-center items-center text-center'>
           hello
         </div>
-        <div className='basis-2/3 h-full bg-black'>
+        <div className='basis-2/3 h-full rounded-xl bg-white justify-center items-center '>
 
         </div>
       </div>
