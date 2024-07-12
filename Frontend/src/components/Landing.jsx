e69dddd762ca049a9c53704fa17edb32a23fce3a
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-12 11:30:27 +0500

    changes

--- Frontend/src/components/Landing.jsx
+++ Frontend/src/components/Landing.jsx
@@ -3,20 +3,18 @@ import img from "../assets/foofpic.png";
 function Landing() {
   return (
     <>
-    <div className="inner-container">
-
-   <div className="text">
-
-      <h1 className="landing-title">Welcome to Cafe Monitoring System</h1>
-      <p className="landing-description">
-        This is a system that helps you monitor your cafe and manage your
-        customers efficiently.
-      </p>
-   </div>
-      <div className="pic">
-        <img src={img} alt="" />
+      <div className="inner-container">
+        <div className="text">
+          <h1 className="landing-title">Welcome to Cafe Monitoring System</h1>
+          <p className="landing-description">
+            This is a system that helps you monitor your cafe and manage your
+            customers efficiently.
+          </p>
+        </div>
+        <div className="pic">
+          <img src={img} alt="" />
+        </div>
       </div>
-    </div>
     </>
   );
 }
