d2e0f51d5803db69eb9f52e684b05fee8fbff604
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-12 15:08:34 +0500

    logo in home page

--- Frontend/src/components/LandingPage/LandingPage.jsx
+++ Frontend/src/components/LandingPage/LandingPage.jsx
@@ -2,12 +2,13 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Landing from "../Landing/Landing";
+import img1 from '../../assets/logo.png';
 import "./LandingPage.css";
 const LandingPage = () => {
   return (
     <div className="bluecontainer">
       <div className="logoname">
-     <h1>Contour Software</h1>
+        <img src={img1} alt="Contour Software Logo" className="logo" />
       </div>
       <div className="landing-container">
         <Navbar />
--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -33,19 +33,10 @@ function Login() {
               <span className="button__text">Log In</span>
               <i className="button__icon fas fa-chevron-right"></i>
             </button>
-            {/* <p className='alert'>Create account </p><Link to="/SignUp" className="login__forgot">Signup</Link> */}
             <br />
             <br />
             <input type="checkbox" id="remember" name="remember" /> Remember Me
           </form>
-          {/* <div className="social-login">
-        <h3>log in via</h3>
-        <div className="social-icons">
-          <a href="#" className="social-login__icon fab fa-instagram"></a>
-          <a href="#" className="social-login__icon fab fa-facebook"></a>
-          <a href="#" className="social-login__icon fab fa-twitter"></a>
-        </div>
-      </div> */}
         </div>
         <div className="screen__background">
           <span className="screen__background__shape screen__background__shape4"></span>
