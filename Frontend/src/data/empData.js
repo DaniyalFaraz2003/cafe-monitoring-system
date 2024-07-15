fda6c9388cf89ef97d5cca516e012717fb7900c7
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-07-15 10:56:06 +0500

    avl fixes

--- Frontend/src/data/empData.js
+++ Frontend/src/data/empData.js
@@ -7,7 +7,7 @@ const employees = [
         city: "Islamabad",
         age: 23,
         mealtype:"Normal",
-        mealtime:"12:30:00 pm",
+        mealtime:"12:30:00",
         mealdate:"2022-10-10"
     },
     {
@@ -18,7 +18,7 @@ const employees = [
         city: "Karachi",
         age: 24,
         mealtype:"Normal",
-        mealtime:"12:30:00 pm",
+        mealtime:"12:30:00",
         mealdate:"2022-08-10"
 
     },
@@ -30,7 +30,7 @@ const employees = [
         city: "Islamabad",
         age: 42,
         mealtype:"Normal",
-        mealtime:"02:30:00 pm",
+        mealtime:"02:30:00",
         mealdate:"2022-10-10"
 
     },
@@ -42,7 +42,7 @@ const employees = [
         city: "Karachi",
         age: 24,
         mealtype:"Diet",
-        mealtime:"12:40:00 pm",
+        mealtime:"12:40:00",
         mealdate:"2022-10-20"
 
     },
@@ -54,10 +54,65 @@ const employees = [
         city: "Lahore",
         age: 48,
         mealtype:"Diet",
-        mealtime:"01:30:00 pm",
+        mealtime:"01:30:00",
         mealdate:"2022-11-10"
 
-    }
+    },
+    {
+        id: 1,
+        name: "John Doe",
+        designation: "Software Engineer",
+        department: "Development",
+        city: "Islamabad",
+        age: 23,
+        mealtype:"Diet",
+        mealtime:"12:30:00",
+        mealdate:"2022-10-10"
+    },
+    {
+        id: 5,
+        name: "Charlie Davis",
+        designation: "Data Analyst",
+        department: "Analytics",
+        city: "Lahore",
+        age: 48,
+        mealtype:"Diet",
+        mealtime:"02:30:00",
+        mealdate:"2022-11-10"
+    },
+    {
+        id: 5,
+        name: "Charlie Davis",
+        designation: "Data Analyst",
+        department: "Analytics",
+        city: "Lahore",
+        age: 48,
+        mealtype:"Diet",
+        mealtime:"02:30:00",
+        mealdate:"2022-11-10"
+    },
+    {
+        id: 5,
+        name: "Charlie Davis",
+        designation: "Data Analyst",
+        department: "Analytics",
+        city: "Lahore",
+        age: 48,
+        mealtype:"Diet",
+        mealtime:"02:30:00",
+        mealdate:"2022-11-10"
+    },
+    {
+        id: 5,
+        name: "Charlie Davis",
+        designation: "Data Analyst",
+        department: "Analytics",
+        city: "Lahore",
+        age: 48,
+        mealtype:"Diet",
+        mealtime:"02:30:00",
+        mealdate:"2022-11-10"
+    },
 ];
 
 export default employees;  
