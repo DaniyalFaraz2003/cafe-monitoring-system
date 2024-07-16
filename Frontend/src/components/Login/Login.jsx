e6c13eabe70c66e8050b7315c50b3a4fabd151f6
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-16 18:34:42 +0500

    changes

--- Frontend/src/components/Login/Login.jsx
+++ Frontend/src/components/Login/Login.jsx
@@ -1,34 +1,35 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import { signin } from "../../redux/avltreeReducer";
 import { useDispatch } from "react-redux";
 import "./Login.css";
-
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const login = async () => {
-
     try {
-      const response = await axios.post("http://localhost:5000/api/v1/login", { username, password })
-      const {message, city} = response.data;
+      const response = await axios.post("http://localhost:5000/api/v1/login", {
+        username,
+        password,
+      });
+      const { message, city } = response.data;
       if (message === "ok") {
         dispatch(signin(city));
-        navigate('/UserEntryForm')
+        navigate("/UserEntryForm");
       } else {
         setMessage("Incorrect Username Or Password");
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
     <div className="main_container">
       <div className="container">
