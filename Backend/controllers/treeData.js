01e947ad6056b44b356d0c96683cfe415e24d18a
Muhammad Zohaib <mzhscorpio1@gmail.com>
2024-07-17 16:18:12 +0500

    report city fix

--- Backend/controllers/treeData.js
+++ Backend/controllers/treeData.js
@@ -4,7 +4,7 @@ const getTreeData = async (req, res) => {
     const city = req.params['city']
     try {
         const [result] = await db.query(`
-            SELECT m.Emp_ID as id, e.Emp_Name as name, e.Designation as designation, e.Dept as department, e.City as city, e.Age as age, m.meal_pref as mealtype, m.meal_time as mealtime, m.meal_date as mealdate
+            SELECT m.Emp_ID as id, e.Emp_Name as name, e.Designation as designation, e.Dept as department, m.City as city, e.Age as age, m.meal_pref as mealtype, m.meal_time as mealtime, m.meal_date as mealdate
             FROM meal_record m
             JOIN Employee e
             ON m.Emp_ID = e.Emp_ID
