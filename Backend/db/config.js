9df8c514c83bf0674d81bc40937d8989bb22ad29
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-17 16:21:34 +0500

    changes

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise")
 const mysqlPool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "password",
-    database: "cafe_project",
+    password: "kyasekya90",
+    database: "CAFEE",
 })
 
 module.exports = mysqlPool;
