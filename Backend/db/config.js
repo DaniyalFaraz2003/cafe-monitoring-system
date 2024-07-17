c8ecd345fee4cbfd13ed2626a6aac8dfeedb0284
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2024-07-17 19:04:06 +0500

    minor changes

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,6 +3,8 @@ const mysql = require("mysql2/promise")
 const mysqlPool = mysql.createPool({
     host: "localhost",
     user: "root",
+    password: "daniyalmysql",
+    database: "cafe_project"
 })
 
 module.exports = mysqlPool;
