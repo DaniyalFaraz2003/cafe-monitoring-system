5ed59f242f9b3dda1f9cdcee39d4e4daa0308740
Ummekulsoom <ummekulsoom215@gmail.com>
2024-07-18 17:40:24 +0500

    meal capacity form added

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise")
 const mysqlPool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "kyasekya90",
+    password: "password",
     database: "mycafe_project",
 })
 
