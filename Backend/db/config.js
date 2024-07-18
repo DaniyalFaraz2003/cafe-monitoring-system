bc6eddb03c27a5a566c50e4ae66fb99cf8efddc2
Muhammad Zohaib <mzhscorpio1@gmail.com>
2024-07-18 16:32:07 +0500

    temp

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,13 +3,8 @@ const mysql = require("mysql2/promise")
 const mysqlPool = mysql.createPool({
     host: "localhost",
     user: "root",
-<<<<<<< HEAD
     password: "Pakistan12345",
-    database: "cafe_project"
-=======
-    password: "kyasekya90",
-    database: "mycafe_project",
->>>>>>> a24c1004447fd9c3cb5cb176c7a1e6036a0e1f85
+    database: "cafe_project",
 })
 
 module.exports = mysqlPool;
