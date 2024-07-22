021e1684359e8c9a249c91ecdad4981bdb1da355
Kulsoom <ummeKulsoom215@gmail.com>
2024-07-22 11:39:38 +0500

    changes

--- Backend/db/config.js
+++ Backend/db/config.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise")
 const mysqlPool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Pakistan12345",
+    password: "kyasekyahogaye",
     database: "cafe_project"
 })
 
